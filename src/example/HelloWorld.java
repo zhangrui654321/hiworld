28b2fa2b8160c550b55e1fecde1196ee19b734e1
15172537049@163.com <zr525010>
2020-05-30 23:29:25 +0800

    创建成功

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
   }
   public static void main(String[] argv) {
     Object implementor = new HelloWorld ();
-    String address = "http://localhost:9000/HelloWorlds";
+    String address = "http://localhost:9000/HelloWorldss";
     Endpoint.publish(address, implementor);
   }
 }
